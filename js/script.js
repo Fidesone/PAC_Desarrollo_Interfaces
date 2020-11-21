3bf26302c618fc89aa062d6b4f4843eeac0fe4c4
Héctor González Pereda <51021644+Fidesone@users.noreply.github.com>
2020-11-21 19:44:18 +0100

    add function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
-var bolasCreadas;
-var bolasEliminadas;
-var color = ["blue", "red", "yellow", "green", "purple"];
+$(document).ready(function(){
+    $("#add").click(function(){
+        
+        $("#main").append("<div class='ball'></div>")
+             
+        });
+        
+    });
 
-function adornar(){
-    var bola =$('<div class"bola"></div>')
-    var positionTop = randomRange(50,300);
-    var positionLeft = 0;
 
-    if (positionTop >=50 && positionTop <=90){
-        var positionLeft=randomRange(190, 230);
-    } else if(posicionTop > 80 && posicionTop <= 130) {
-        
-        var posicionLeft = randomRange(170, 260);
-    } else if (posicionTop > 130 && posicionTop <= 220) {
-        
-        var posicionLeft = randomRange(136, 265);
-        
-    } else if (posicionTop > 210 && posicionTop <= 320) {
-        
-        var posicionLeft = randomRange(115, 300);
-}
+    function addball(){
+        var contador=0;
+    }
 
-}
+    $('#remove').click(function(){
+        $("#img").effect("shake")
+
+
+
+    var posX=0;
+    var posY=0;
+
+    //llamada a las funciones
+
+
+
+
+
+    function changeposition(){
+        posX=(Math.random() * 500) + 1;
+        posY= (Math.random() * 500) +1;
+        $("#bola").animate({"left": posX, "top":posY})
+    }
+
+    //setInterval(changeposition, 2000);
+})
+
+
+
+
+
+
+ 
